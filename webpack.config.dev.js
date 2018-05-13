f32578492a3f9a0029c6e87da5fa66c72bae86cb
Stefan Piazza <stefanpiazza@gmail.com>
2018-05-13 19:13:38 +0100

    CHORE: build files

---
+++ webpack.config.dev.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    devServer: {
+    	contentBase: 'dist/',
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        publicPath: '/'
+    },
+    devtool: 'source-map',
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '/static/images/[name].[ext]'
+                }
+            }, {
+                loader: 'image-webpack-loader'
+            }]
+        }, {
+            test: /\.scss$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader',
+                options: {
+                    camelCase: true,
+                    // Keep same as class definition for now
+                    localIdentName: '[local]',
+                    importLoaders: 2,
+                    modules: true,
+                    sourceMap: true
+                }
+            }, {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            }, {
+                loader: 'sass-loader',
+                options: {
+                    sourceMap: true
+                }
+            }]
+        }]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                common: {
+                    chunks: "initial",
+                    test: path.resolve(__dirname, "node_modules"),
+                    name: "common",
+                    enforce: true
+                }
+            }
+        }
+    },
+    output: {
+        chunkFilename: 'static/scripts/[name].js',
+        filename: 'static/scripts/[name].js',
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            chunks: ['common', 'app'],
+            filename: 'index.html',
+            template: 'src/index.html',
+            title: 'CSS Grid Menu'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+}
---
+++ webpack.config.prod.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    mode: 'production',
+    module: {
+        rules: [{
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    name: '/static/images/[name].[ext]',
+                    publicPath: '../../'
+                }
+            }, {
+                loader: 'image-webpack-loader'
+            }]
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [{
+                    loader: 'css-loader',
+                    options: {
+                        camelCase: true,
+                        // Keep same as class definition for now
+                        localIdentName: '[local]',
+                        importLoaders: 2,
+                        minimize: true,
+                        modules: true,
+                        sourceMap: false
+                    }
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        sourceMap: false
+                    }
+                }, {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: false
+                    }
+                }]
+            })
+        }]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                common: {
+                    chunks: "initial",
+                    test: path.resolve(__dirname, "node_modules"),
+                    name: "common",
+                    enforce: true
+                }
+            }
+        }
+    },
+    output: {
+        chunkFilename: 'static/scripts/[name].js',
+        filename: 'static/scripts/[name].js',
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/'
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            allChunks: true,
+            filename: 'static/styles/[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            chunks: [],
+            filename: 'index.html',
+            template: 'src/index.html',
+            title: 'CSS Grid Menu'
+        })
+    ]
+}
